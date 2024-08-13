09cd079a6749b420f683549aef4c207a52489939
Siddharth Kundu <sk31395@gmail.com>
2024-08-13 19:21:06 -0400

    remove comments

--- src/app/auth/signin/signin.tsx
+++ src/app/auth/signin/signin.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { GithubButton } from "@/components/Button/GithubButton";
-import { Button } from "@/components/ui/button";
 import {
   CardHeader,
   CardTitle,
@@ -9,10 +8,7 @@ import {
   CardFooter,
   Card,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import { handleGithubSignIn } from "@/lib/auth/githubSigninServerAction";
-import { Label } from "@radix-ui/react-label";
-import Link from "next/link";
 
 export const SignInPage = () => {
   return (
@@ -22,45 +18,13 @@ export const SignInPage = () => {
           <CardTitle>Sign in to ChatterMind</CardTitle>
         </CardHeader>
         <CardContent>
-          {/* <form action={signInWithCredentials} className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Enter your email"
-                required
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                placeholder="Enter your password"
-                required
-              />
-            </div>
-            <Button type="submit" className="w-full">
-              Sign in
-            </Button>
-          </form> */}
           <div className="mt-4">
             <GithubButton onClick={() => handleGithubSignIn()}>
               Sign in with GitHub
             </GithubButton>
           </div>
         </CardContent>
-        <CardFooter>
-          {/* <p className="text-sm text-center w-full">
-            Don&apos;t have an account?{" "}
-            <Link href="/register" className="text-blue-500 hover:underline">
-              Register here
-            </Link>
-          </p> */}
-        </CardFooter>
+        <CardFooter></CardFooter>
       </Card>
     </div>
   );
